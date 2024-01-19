101ca86787c65713944f7cedfd1c4dac379e9c46
Sam Selikoff <sam.selikoff@gmail.com>
2024-01-19 14:09:30 -0500

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,81 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import * as Slider from "@radix-ui/react-slider";
+import {
+  animate,
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  useSpring,
+  useTransform,
+} from "framer-motion";
+
+export default function Page() {
+  let pixels = useMotionValue(0);
+  // let scaleX = useMotionValue(1);
+  // let pixels = useSpring(pixelsRaw, { damping: 30, mass: 1, stiffness: 500 });
+  let scaleX = useTransform(pixels, [0, -100], [100, 130]);
+  let marginLeftRaw = useTransform(pixels, [0, -100], [0, -30]);
+  // let scaleX = useSpring(scaleXRaw, { mass: 1, stiffness: 100 });
+  let xRaw = useTransform(scaleX, (v) => (v - 1) * 100);
+  let width = useMotionTemplate`${scaleX}%`;
+  let x = useMotionTemplate`${xRaw}%`;
+  let marginLeft = useMotionTemplate`${marginLeftRaw}%`;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="w-80">
+        <motion.div>
+          <motion.div style={{ width, marginLeft }}>
+            <Slider.Root
+              onPointerMove={(e) => {
+                if (e.buttons > 0) {
+                  let { x } = e.currentTarget.getBoundingClientRect();
+                  let diff = e.clientX - x;
+                  console.log(diff);
+                  pixels.stop();
+                  pixels.set(diff);
+                  // if (diff < 0) {
+                  //   scaleX.set(1.05);
+                  //   // scale.set;
+                  // } else {
+                  //   scaleX.set(1);
+                  // }
+                  // let diffInPixels = e.clientX - startingValues.current.x;
+                  // let widthInPixels = ref.current.getBoundingClientRect().width;
+                  // let diffInValue = diffInPixels / (widthInPixels / (max - min));
+                  // let newValue = startingValues.current.value + diffInValue;
+                  // let valueToStep = roundToStep(newValue, step);
+                  // let clampedValue = clamp(valueToStep, min, max);
+                  // updateValue(clampedValue);
+                } else {
+                  // console.log("here");
+                  animate(pixels, 0, {
+                    type: "spring",
+                    bounce: 0,
+                    duration: 0.5,
+                  });
+                }
+              }}
+              onPointerUp={() => {
+                // pixels.set(0);
+                console.log("h");
+                animate(pixels, 0, {
+                  type: "spring",
+                  bounce: 0,
+                  duration: 0.5,
+                });
+              }}
+              className="flex relative w-full h-4"
+            >
+              <Slider.Track className="bg-gray-400 grow">
+                <Slider.Range className="bg-gray-800 absolute h-full" />
+              </Slider.Track>
+              <Slider.Thumb />
+            </Slider.Root>
+          </motion.div>
+        </motion.div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
