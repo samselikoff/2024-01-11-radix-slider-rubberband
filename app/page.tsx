46867335b030c33eeee28ec44e56c9de9b80ec7b
Sam Selikoff <sam.selikoff@gmail.com>
2024-01-23 13:45:05 -0500

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -14,93 +14,66 @@ import {
 } from "framer-motion";
 import { useEffect } from "react";
 
-// function decay(x) {
-//   return -Math.exp(Math.abs(x)) + 1;
-// }
-
-function sigmoid(x: number) {
+// sigmoid function
+function decay(x: number) {
   return 1 / (1 + Math.exp(-x)) - 0.5;
 }
 
+const OVERFLOW_PIXELS = 75;
+const STRETCH_PERCENTAGE = 0.75;
+
 export default function Page() {
   let pixels = useMotionValue(0);
-  // let scaleX = useMotionValue(1);
-  // let pixels = useSpring(pixelsRaw, { damping: 30, mass: 1, stiffness: 500 });
 
-  let overflowAmount = 0.75;
-  let overflowPixels = 75;
-
-  let overflowR = useTransform(pixels, [0, -overflowPixels], [0, 1], {
+  let overflowProgress = useTransform(pixels, [0, -OVERFLOW_PIXELS], [0, 1], {
     clamp: false,
   });
-  let overflow = useTransform(overflowR, sigmoid);
+  let overflowProgressDecayed = useTransform(overflowProgress, decay);
 
-  let scaleX = useTransform(overflow, [0, 1], [1, 1 + overflowAmount], {
-    clamp: false,
-  });
-  let scaleY = useTransform(overflow, [0, 1], [1, 0.75], { clamp: false });
-  let translateX = useTransform(
-    overflow,
+  let scaleX = useTransform(
+    overflowProgressDecayed,
     [0, 1],
-    ["0%", `-${(overflowAmount * 100) / 2}%`],
+    [1, 1 + STRETCH_PERCENTAGE],
+    {
+      clamp: false,
+    },
+  );
+  let scaleY = useTransform(overflowProgressDecayed, [0, 1], [1, 0.75], {
+    clamp: false,
+  });
+  let translateX = useTransform(
+    overflowProgressDecayed,
+    [0, 1],
+    ["0%", `-${(STRETCH_PERCENTAGE * 100) / 2}%`],
     { clamp: false },
   );
-
-  // useEffect(() => {
-  //   overflowR.on("change", (v) => {
-  //     console.log(v, overflow.get());
-  //   });
-  // });
-
-  // old
-  // let scaleX = useTransform(pixels, [0, -100], [100, 130]);
-  // let marginLeftRaw = useTransform(pixels, [0, -100], [0, -30]);
-  // let xRaw = useTransform(scaleX, (v) => (v - 1) * 100);
-  // let width = useMotionTemplate`${scaleX}%`;
-  // let x = useMotionTemplate`${xRaw}%`;
-  // let marginLeft = useMotionTemplate`${marginLeftRaw}%`;
 
   return (
     <div
       onLostPointerCaptureCapture={() => {
-        console.log("onlost - root div!");
         animate(pixels, 0, {
           type: "spring",
           bounce: 0.75,
           duration: 2,
         });
       }}
-      // onPointerUp={() => {
-      //   console.log("onpointerup - root div!");
-      //   animate(pixels, 0, {
-      //     type: "spring",
-      //     bounce: 0,
-      //     duration: 0.2,
-      //   });
-      // }}
       className="flex min-h-screen items-center justify-center bg-gray-950"
     >
       <div className="w-60">
         <div className="relative">
-          <div className="~border border-black~ pointer-events-none absolute inset-0 z-10"></div>
-          {/* <motion.div style={{ width, marginLeft }}> */}
-          {/* <motion.div style={{ scaleX }}> */}
+          <div className="~border pointer-events-none absolute inset-0 z-10"></div>
           <motion.div
             style={{
               scaleX,
-              scaleY,
+              scaleY: 0.5,
               translateX,
-              borderRadius: 9999,
+              borderRadius: 3,
               overflow: "hidden",
             }}
             className="relative z-50"
           >
-            {/* <motion.div style={{ scaleX: 1.5, translateX: "-25%" }}> */}
             <Slider.Root
               step={0.1}
-              onPointerDown={() => {
-                // pixels.stop();
-              }}
               onPointerMove={(e) => {
                 if (e.buttons > 0) {
                   // console.log(e);
