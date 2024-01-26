e0c7b34f14b70887f94ee14484a8ced07f1788d5
Sam Selikoff <sam.selikoff@gmail.com>
2024-01-25 23:33:00 -0500

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,21 @@
 import { SpeakerWaveIcon, SpeakerXMarkIcon } from "@heroicons/react/20/solid";
 import * as Slider from "@radix-ui/react-slider";
 import {
-  MotionConfig,
   MotionValue,
   animate,
   motion,
   useMotionValue,
   useMotionValueEvent,
-  useSpring,
   useTransform,
 } from "framer-motion";
-import { CSSProperties, ElementRef, useRef, useState } from "react";
+import { ElementRef, useRef, useState } from "react";
 
 // Sigmoid function. Output is between 0 and 1.
 function decay(x: number) {
   return 1 / (1 + Math.exp(-x)) - 0.5;
 }
 
-const MAX_PIXELS = 150;
-const SOME_WIDTH = 320;
+const MAX_PIXELS = 50;
 
 export default function Page() {
   let [volume, setVolume] = useState(50);
@@ -34,17 +31,20 @@ export default function Page() {
   useMotionValueEvent(clientX, "change", (latest) => {
     if (ref.current) {
       let { x, width } = ref.current.getBoundingClientRect();
+      let newOverflow: number;
 
       if (latest < x) {
         setBounds("left");
-        pixelsOverflow.set(x - latest);
+        newOverflow = x - latest;
       } else if (latest > x + width) {
         setBounds("right");
-        pixelsOverflow.set(latest - x - width);
+        newOverflow = latest - x - width;
       } else {
         setBounds("initial");
-        pixelsOverflow.set(0);
+        newOverflow = 0;
       }
+
+      pixelsOverflow.set(decay(newOverflow / MAX_PIXELS) * MAX_PIXELS);
     }
   });
 
@@ -75,7 +75,7 @@ export default function Page() {
                 ),
               }}
             >
-              <SpeakerXMarkIcon className="size-6 text-white" />
+              <SpeakerXMarkIcon className="size-5 text-white" />
             </motion.div>
 
             <Slider.Root
@@ -83,30 +83,44 @@ export default function Page() {
               onValueChange={([v]) => setVolume(v)}
               ref={ref}
               onLostPointerCapture={() => {
-                animate(pixelsOverflow, 0, { type: "spring" });
+                animate(pixelsOverflow, 0, { type: "spring", bounce: 0.5 });
               }}
               onPointerMove={(e) => {
                 if (e.buttons > 0) {
                   clientX.set(e.clientX);
                 }
               }}
-              className="~border relative flex w-full grow cursor-grab touch-none items-center border-red-500 py-4 active:cursor-grabbing "
+              className="relative flex w-full grow cursor-grab touch-none items-center py-4 active:cursor-grabbing"
             >
               <motion.div
                 style={{
-                  scaleX: useTransform(
-                    () => 1 + pixelsOverflow.get() / SOME_WIDTH,
-                  ),
-                  transformOrigin: bounds === "left" ? "right" : "left",
+                  scaleX: useTransform(() => {
+                    if (!ref.current) {
+                      return 1;
+                    }
+
+                    return 1 + pixelsOverflow.get() / ref.current.clientWidth;
+                  }),
+                  scaleY: useTransform(() => {
+                    return 1 - pixelsOverflow.get() / MAX_PIXELS;
+                  }),
+                  transformOrigin: useTransform(() => {
+                    if (ref.current) {
+                      let { x, width } = ref.current.getBoundingClientRect();
+                      return clientX.get() < x + width / 2 ? "right" : "left";
+                    }
+                  }),
                   height: 6,
                 }}
                 variants={{
                   hovered: {
-                    height: 12,
-                    transition: { type: "spring", bounce: 0, duration: 0.4 },
+                    height: 16,
+                    marginTop: -5,
+                    marginBottom: -5,
                   },
                 }}
-                className="relative flex grow"
+                transition={{ type: "spring", bounce: 0, duration: 0.4 }}
+                className="flex grow"
               >
                 <Slider.Track className="relative h-full grow overflow-hidden rounded-full bg-white">
                   <Slider.Range className="absolute h-full bg-sky-500" />
@@ -122,16 +136,20 @@ export default function Page() {
               }}
               style={{ translateX: bounds === "right" ? pixelsOverflow : 0 }}
             >
-              <SpeakerWaveIcon className="size-6 text-white" />
+              <SpeakerWaveIcon className="size-5 text-white" />
             </motion.div>
           </div>
         </motion.div>
       </div>
 
+      <p className="mt-1 text-center font-medium">
+        Volume: <span className="tabular-nums">{volume}</span>
+      </p>
+
       <div className="mt-8 w-full max-w-sm text-left">
-        <Debug label="pixelsOverflow" motionValue={pixelsOverflow} />
+        {/* <Debug label="pixelsOverflow" motionValue={pixelsOverflow} />
         <Debug label="clientX" motionValue={clientX} />
-        <p>Bounds: {bounds}</p>
+        <p>Bounds: {bounds}</p> */}
       </div>
     </div>
   );
