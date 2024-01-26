f45151fc6ddd2bb7250eb5dfae3e2e1470c8a082
Sam Selikoff <sam.selikoff@gmail.com>
2024-01-25 22:50:54 -0500

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -31,21 +31,6 @@ export default function Page() {
   let clientX = useMotionValue(0);
   let pixelsOverflow = useMotionValue(0);
 
-  // Animated derived
-  // let pixelsOverflow = useTransform(() => {
-  //   if (ref.current) {
-  //     let { x, width } = ref.current.getBoundingClientRect();
-  //     let current = clientX.get();
-  //     let overflow = Math.abs(
-  //       current - Math.min(x + width, Math.max(x, current)),
-  //     );
-
-  //     return overflow;
-  //   } else {
-  //     return 0;
-  //   }
-  // });
-
   useMotionValueEvent(clientX, "change", (latest) => {
     if (ref.current) {
       let { x, width } = ref.current.getBoundingClientRect();
@@ -63,90 +48,88 @@ export default function Page() {
     }
   });
 
-  console.log("render");
-
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <div className="w-full">
-        <MotionConfig transition={{ type: "spring", bounce: 0, duration: 0.4 }}>
-          <motion.div
-            whileHover="hovered"
-            style={{ "--height": "6px", scale: 1 } as CSSProperties}
-            className="~bg-gray-900 flex cursor-grab justify-center p-3 active:cursor-grabbing"
-            initial={false}
-            animate={bounds}
-            variants={{
-              hovered: {
-                scale: 1.1,
-              },
-            }}
-          >
-            <div className="flex w-full max-w-sm items-center gap-3">
+        <motion.div
+          whileHover="hovered"
+          className="~bg-gray-900 flex justify-center"
+          initial={false}
+          animate={bounds}
+          variants={{
+            hovered: {
+              scale: 1.1,
+              transition: { type: "spring", bounce: 0, duration: 0.4 },
+            },
+          }}
+        >
+          <div className="flex w-full max-w-sm items-center gap-3">
+            <motion.div
+              animate={{
+                scale: bounds === "left" ? [1, 1.4, 1] : 1,
+                transition: { duration: 0.25 },
+              }}
+              style={{
+                translateX: useTransform(() =>
+                  bounds === "left" ? -pixelsOverflow.get() : 0,
+                ),
+              }}
+            >
+              <SpeakerXMarkIcon className="size-6 text-white" />
+            </motion.div>
+
+            <Slider.Root
+              value={[volume]}
+              onValueChange={([v]) => setVolume(v)}
+              ref={ref}
+              onLostPointerCapture={() => {
+                animate(pixelsOverflow, 0, { type: "spring" });
+              }}
+              onPointerMove={(e) => {
+                if (e.buttons > 0) {
+                  clientX.set(e.clientX);
+                }
+              }}
+              className="~border relative flex w-full grow cursor-grab touch-none items-center border-red-500 py-4 active:cursor-grabbing "
+            >
               <motion.div
-                variants={{
-                  left: { scale: [1, 1.4, 1], transition: { duration: 0.25 } },
-                }}
                 style={{
-                  translateX: useTransform(() =>
-                    bounds === "left" ? -pixelsOverflow.get() : 0,
+                  scaleX: useTransform(
+                    () => 1 + pixelsOverflow.get() / SOME_WIDTH,
                   ),
+                  transformOrigin: bounds === "left" ? "right" : "left",
+                  height: 6,
                 }}
-              >
-                <SpeakerXMarkIcon className="size-5 text-white" />
-              </motion.div>
-
-              <Slider.Root
-                value={[volume]}
-                onValueChange={([v]) => setVolume(v)}
-                ref={ref}
-                onLostPointerCapture={() => {
-                  animate(pixelsOverflow, 0, { type: "spring" });
-                }}
-                onPointerMove={(e) => {
-                  if (e.buttons > 0) {
-                    clientX.set(e.clientX);
-                  }
-                }}
-                className="~border border-red-500~ relative flex w-full grow touch-none items-center py-2 "
-              >
-                <motion.div
-                  style={{
-                    scaleX: useTransform(
-                      () => 1 + pixelsOverflow.get() / SOME_WIDTH,
-                    ),
-                    transformOrigin: bounds === "left" ? "right" : "left",
-                    height: 6,
-                  }}
-                  variants={{
-                    hovered: {
-                      height: 12,
-                    },
-                  }}
-                  className="relative flex grow"
-                >
-                  <Slider.Track className="relative h-full grow overflow-hidden rounded-full bg-white">
-                    <Slider.Range className="absolute h-full bg-sky-500" />
-                  </Slider.Track>
-                </motion.div>
-                <Slider.Thumb />
-              </Slider.Root>
-
-              <motion.div
                 variants={{
-                  right: { scale: [1, 1.4, 1], transition: { duration: 0.25 } },
+                  hovered: {
+                    height: 12,
+                    transition: { type: "spring", bounce: 0, duration: 0.4 },
+                  },
                 }}
-                style={{ translateX: bounds === "right" ? pixelsOverflow : 0 }}
+                className="relative flex grow"
               >
-                <SpeakerWaveIcon className="size-5 text-white" />
+                <Slider.Track className="relative h-full grow overflow-hidden rounded-full bg-white">
+                  <Slider.Range className="absolute h-full bg-sky-500" />
+                </Slider.Track>
               </motion.div>
-            </div>
-          </motion.div>
-        </MotionConfig>
+              <Slider.Thumb />
+            </Slider.Root>
+
+            <motion.div
+              animate={{
+                scale: bounds === "right" ? [1, 1.4, 1] : 1,
+                transition: { duration: 0.25 },
+              }}
+              style={{ translateX: bounds === "right" ? pixelsOverflow : 0 }}
+            >
+              <SpeakerWaveIcon className="size-6 text-white" />
+            </motion.div>
+          </div>
+        </motion.div>
       </div>
 
       <div className="mt-8 w-full max-w-sm text-left">
         <Debug label="pixelsOverflow" motionValue={pixelsOverflow} />
-        {/* <Debug label="pixelsOverflowNew" motionValue={pixelsOverflowNew} /> */}
         <Debug label="clientX" motionValue={clientX} />
         <p>Bounds: {bounds}</p>
       </div>
